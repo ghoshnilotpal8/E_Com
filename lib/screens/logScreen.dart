f2930b72b9fbbb06de6ee843346b57032d19d105
Rohit ranjan <ranjanrohit812@gmail.com>
2021-11-01 17:38:54 +0530

    Auth Screen done

---
+++ lib/screens/logScreen.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:vrouter/src/core/extended_context.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({Key? key}) : super(key: key);
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final _formKey1 = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Welcome Back!',
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 16.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0.sp, bottom: 20.sp),
+                  child: Text(
+                    'Please enter your account here',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15.sp,
+                    ),
+                  ),
+                ),
+                Form(
+                  key: _formKey1,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'This field cannot by empty';
+                            }
+                            if (!value.contains('@')) {
+                              return 'Please enter valid email';
+                            }
+                            return null;
+                          },
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.email_outlined,
+                              color: Colors.black,
+                            ),
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'This field cannot by empty';
+                            }
+                            return null;
+                          },
+                          obscureText: true,
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                            prefixIcon:
+                                Icon(Icons.lock_outline, color: Colors.black),
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {
+                            //forgot password screen
+                          },
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25.h,
+                      ),
+                      Container(
+                        height: 65,
+                        width: double.infinity,
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.green),
+                          ),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(fontSize: 18.sp),
+                          ),
+                          onPressed: () {
+                            _formKey1.currentState!.validate();
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0.h, bottom: 20.0.h),
+                        child: Text(
+                          'Or Continue with',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.sp,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 65,
+                        width: double.infinity,
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.red),
+                          ),
+                          child: Text(
+                            'Google',
+                            style: TextStyle(fontSize: 18.sp),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            'Already Have an account?',
+                            style: TextStyle(
+                              fontSize: 15.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextButton(
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontSize: 15.sp,
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onPressed: () {
+                              context.vRouter.to('/signUp');
+                            },
+                          )
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
